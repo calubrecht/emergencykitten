831c6d122d15cc07220cadfc4a429a0e3b4719c0
Chad Lubrecht <chad@calubrecht.us>
2016-03-23 00:15:34 -0400

    Allow php to work with utf8, since some existing data is utf 8

--- kittens.js.php
+++ kittens.js.php
@@ -2,21 +2,21 @@
 $servername = "localhost";
 $username = "usernameHere";
 $password = "passwordHere";
-$database = "dbHere
+$database = "dbHere";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=".$database, $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=" . $database . ";charset=utf8", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $conn->prepare("SELECT url, title, imageUrl, imageAlt from links");
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     $arr = $stmt->fetchAll();
-    print "var kittens = " .(json_encode($arr));
+    print "var kittens = " .(json_encode($arr)). ";";
     }
 catch(PDOException $e)
     {
       $res = array("url"=>"", "title"=>"Connection failed: " . $e->getMessage() ,"imageURL"=>"", "imageAlt"=>"");
-    print "var kittens = " .(json_encode($res));
+    print "var kittens = " .(json_encode($res)) . ";";
     }
 ?>
